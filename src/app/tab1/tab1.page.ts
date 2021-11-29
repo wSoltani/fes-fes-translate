0bfbbd1dac04039ff7a0401b35b3a0a3189cd284
wSoltani <w.soltani@almaviva.tn>
2021-11-29 12:47:16 +0100

    Add Speech Recognition w/ permission request

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { SpeechRecognition } from '@ionic-native/speech-recognition/ngx';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  constructor(private speechRecognition: SpeechRecognition) {
+    // this.speechRecognition
+    //   .isRecognitionAvailable()
+    //   .then((available: boolean) => console.log(available));
+  }
 
-  constructor() {}
-
+  startRecognition() {
+    this.speechRecognition.hasPermission().then((hasPermission: boolean) => {
+      if (hasPermission) {
+        this.speechRecognition.startListening().subscribe(
+          (matches: string[]) => console.log(matches),
+          (onerror) => console.log('error:', onerror)
+        );
+      } else {
+        this.speechRecognition.requestPermission().then(
+          () => this.startRecognition(),
+          () => console.log('Denied')
+        );
+      }
+    });
+  }
 }
